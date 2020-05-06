41d0842f76f9043649042af4e0eeea780264ff18
zhaozhiwen <zhiwen.zhao@ximalaya.com>
2020-05-06 17:41:44 +0800

    feat: 实现Didact.createElement

---
+++ src/didact/createElement.js
@@ -0,0 +1,24 @@
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) => {
+        if (typeof child === "object") return child;
+        return createTextElement(child);
+      }),
+    },
+  };
+}
+
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
+
+export default createElement;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import Didact from "./didact";
+
+const element = (
+  <div id="foo">
+    <h1>hello</h1> <h2>world</h2>
+  </div>
+);
+
+
+console.log(element);
