9613f88b125e45667c05d127667a67b392ec24f2
zhaozhiwen <zhiwen.zhao@ximalaya.com>
2020-05-08 11:32:25 +0800

    feat: Render and Commit Phases

--- src/render.js
+++ src/render.js
@@ -6,7 +6,7 @@ function createDom(fiber) {
   const dom =
     fiber.type === TEXT_ELEMENT
       ? document.createTextNode("")
-      : document.createElement(element.type);
+      : document.createElement(fiber.type);
 
   Object.keys(fiber.props)
     .filter(isProperty)
@@ -17,16 +17,32 @@ function createDom(fiber) {
   return dom;
 }
 
+function commitRoot() {
+  // add nodes to dom
+  commitWork(wipRoot);
+  wipRoot = null;
+}
+
+function commitWork(fiber) {
+  const { parent, child, sibling } = fiber;
+
+  if (parent) parent.dom.appendChild(fiber.dom);
+  if (child) commitWork(child);
+  if (sibling) commitWork(sibling);
+}
+
 function render(element, container) {
-  nextUnitOfWork = {
+  wipRoot = {
     dom: container,
     props: {
       children: [element],
     },
   };
+  nextUnitOfWork = wipRoot;
 }
 
 let nextUnitOfWork = null;
+let wipRoot = null;
 
 function workLoop(deadline) {
   let shouldYield = false;
@@ -34,19 +50,30 @@ function workLoop(deadline) {
     nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
     shouldYield = deadline.timeRemaining() < 1;
   }
+
+  // 没有下一个工作单元了 将变更提交至DOM
+  if (!nextUnitOfWork && wipRoot) {
+    commitRoot();
+  }
+
   requestIdleCallback(workLoop);
 }
 
 requestIdleCallback(workLoop);
 
 function performUnitOfWork(fiber) {
-  // 1. 将 element 添加至 DOM
+  // // 1. 将 element 添加至 DOM
+  // 1. 创建dom
   if (!fiber.dom) {
     fiber.dom = createDom(fiber);
   }
-  if (fiber.parent) {
-    fiber.parent.dom.appendChild(fiber.dom);
-  }
+
+  // 避免用户看到不完整的UI，不在每一个节点处理过程中就添加至DOM
+  // 而是待所有工作单元处理完毕，再提交变更至DOM
+  // if (fiber.parent) {
+  //   fiber.parent.dom.appendChild(fiber.dom);
+  // }
+
   // 2. 为 element 的 children 创建 fiber 节点
   const elements = fiber.props.children;
 
@@ -72,7 +99,7 @@ function performUnitOfWork(fiber) {
     }
     // 注意是指针移动
     prevSibling = newFiber;
-    i++;
+    index++;
   }
 
   /**
@@ -81,7 +108,7 @@ function performUnitOfWork(fiber) {
    * b) 如果有兄弟节点，返回兄弟节点
    * c) 如果都没有，返回叔叔节点
    * d) 如果没有叔叔节点，沿着父节点向上走，直到找到某个祖先叔叔节点
-   * e) 如果到了根节点还没找到 完事儿了您
+   * e) 如果到了根节点还未找到 结束流程
    */
 
   if (fiber.child) {
